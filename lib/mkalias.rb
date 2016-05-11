97cc54c46aa1c48447d7c31c2cf3b31e4a2e259b
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-11 14:22:01 -0300

    Use #{number} to make args

--- lib/mkalias.rb
+++ lib/mkalias.rb
@@ -3,16 +3,18 @@ require "mkalias/version"
 module Mkalias
 
   def self.new_alias(alias_name, command)
-    bash_path = "#{File.expand_path('~')}/.bashrc"
+    command = command.gsub('#', '$')
 
     function_name = "mkalias_#{alias_name}"
     bash_function = "function #{function_name}(){ #{command}; }"
     bash_alias = "alias #{alias_name}='#{function_name}'"
 
+    bash_path = "#{File.expand_path('~')}/.bashrc"
     open(bash_path, 'a') do |file|
         file.puts("\n")
         file.puts(bash_alias)
         file.puts(bash_function)
     end
   end
+
 end
