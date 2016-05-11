1f499258d5b00a16909e95b764b57f10df62527f
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-11 13:58:07 -0300

    Add new alias

--- lib/mkalias.rb
+++ lib/mkalias.rb
@@ -1,5 +1,13 @@
 require "mkalias/version"
 
 module Mkalias
-  # Your code goes here...
+
+  def self.new_alias(alias_name, command)
+    bash_path = "#{File.expand_path('~')}/.bashrc"
+
+    bash_line = "alias #{alias_name}='#{command}'"
+    open(bash_path, 'a') do |file|
+        file.puts(bash_line)
+    end
+  end
 end
