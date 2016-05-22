fcfa4c551974b947c0fca748e97ba960faf80ac6
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-22 10:43:34 -0300

    Fix rubocop on lib folder

--- lib/mkalias.rb
+++ lib/mkalias.rb
@@ -1,12 +1,12 @@
 require 'mkalias/version'
 require 'set'
 
+# Class to manage alias
 module Mkalias
+  SIGNAL_NAME = 'USR1'.freeze
+  BASHRC_PATH = "#{File.expand_path('~')}/.bashrc".freeze
 
-  SIGNAL_NAME = 'USR1'
-  BASHRC_PATH = "#{File.expand_path('~')}/.bashrc"
-
-  def self.new_alias(alias_name, commands, file_path=BASHRC_PATH)
+  def self.new_alias(alias_name, commands, file_path = BASHRC_PATH)
     alias_names = Mkalias.list_alias(file_path)
     return false if alias_names.include?(alias_name)
 
@@ -17,15 +17,13 @@ module Mkalias
     bash_alias = "alias #{alias_name}='#{function_name}'"
 
     open(file_path, 'a') do |file|
-      file.puts("\n")
-      file.puts(bash_alias)
-      file.puts(bash_function)
+      file.puts("\n#{bash_alias}\n#{bash_function}")
     end
 
-    return true
+    true
   end
 
-  def self.list_alias(file_path=BASHRC_PATH)
+  def self.list_alias(file_path = BASHRC_PATH)
     alias_names = Set.new
 
     alias_regex = /mkalias_(.*?)\(/
@@ -39,21 +37,21 @@ module Mkalias
     alias_names.to_a
   end
 
-  def self.show_alias(alias_names, file_path=BASHRC_PATH)
-    alias_names = [alias_names] unless alias_names.kind_of?(Array)
+  def self.show_alias(alias_names, file_path = BASHRC_PATH)
+    alias_names = [alias_names] unless alias_names.is_a?(Array)
 
     alias_commands = {}
     alias_names.each do |alias_name|
       alias_commands[alias_name] = Mkalias.get_alias_command(alias_name,
-                                                               file_path)
+                                                             file_path)
     end
 
-    alias_commands.select!{ |_, value| !value.nil? }
+    alias_commands.select! { |_, value| !value.nil? }
     alias_commands
   end
 
-  def self.remove_alias(alias_names, file_path=BASHRC_PATH)
-    alias_names = [alias_names] unless alias_names.kind_of?(Array)
+  def self.remove_alias(alias_names, file_path = BASHRC_PATH)
+    alias_names = [alias_names] unless alias_names.is_a?(Array)
 
     removed_alias = []
     alias_names.each do |alias_name|
@@ -64,8 +62,8 @@ module Mkalias
     removed_alias
   end
 
-  def self.add_signal(file_path=BASHRC_PATH)
-    return false if Mkalias.has_signal?(file_path)
+  def self.add_signal(file_path = BASHRC_PATH)
+    return false if Mkalias.signal?(file_path)
 
     trap_command = "trap 'source #{file_path}' #{SIGNAL_NAME}"
     open(file_path, 'a') do |file|
@@ -73,26 +71,26 @@ module Mkalias
       file.puts(trap_command)
     end
 
-    return true
+    true
   end
 
-  def self.remove_signal(file_path=BASHRC_PATH)
-    return false unless has_signal?(file_path)
+  def self.remove_signal(file_path = BASHRC_PATH)
+    return false unless signal?(file_path)
 
     trap_regex = /\btrap\s'source\s(.*)\sUSR1/
 
-    lines = File.readlines(file_path).reject{ |line| line =~ trap_regex }
-    File.open(file_path, "w"){ |f| lines.each { |line| f.puts line } }
+    lines = File.readlines(file_path).reject { |line| line =~ trap_regex }
+    File.open(file_path, 'w') { |f| lines.each { |line| f.puts line } }
 
-    return true
+    true
   end
 
-  def self.has_signal?(file_path=BASHRC_PATH)
+  def self.signal?(file_path = BASHRC_PATH)
     trap_regex = /\btrap\s'source\s(.*)\sUSR1/
     !File.foreach(file_path).grep(trap_regex).empty?
   end
 
-  def self.get_alias_command(alias_name, file_path=BASHRC_PATH)
+  def self.get_alias_command(alias_name, file_path = BASHRC_PATH)
     alias_names = Mkalias.list_alias(file_path)
     return nil unless alias_names.include?(alias_name)
 
@@ -103,29 +101,25 @@ module Mkalias
 
     alias_commands.each do |command|
       result = command.match(command_regex)
-        return result.captures.first.split(';').each{ |c| c.strip! } if result
+      return result.captures.first.split(';').each(&:strip!) if result
     end
-
-    return nil
   end
 
-  def self.remove_one_alias(alias_name, file_path=BASHRC_PATH)
+  def self.remove_one_alias(alias_name, file_path = BASHRC_PATH)
     alias_names = Mkalias.list_alias(file_path)
     return false unless alias_names.include?(alias_name)
 
     alias_regex = /\bmkalias_#{alias_name}[(']/
 
-    lines = File.readlines(file_path).reject{ |line| line =~ alias_regex }
+    lines = File.readlines(file_path).reject { |line| line =~ alias_regex }
 
-    File.open(file_path, "w"){ |f| lines.each { |line| f.puts line } }
-
-    return true
+    File.open(file_path, 'w') { |f| lines.each { |line| f.puts line } }
   end
 
   def self.prepare_commands(commands)
-    commands = commands.join('; ') if commands.kind_of?(Array)
-    unless commands.include?(';') or commands.include?('#')
-        commands = "#{commands} $@"
+    commands = commands.join('; ') if commands.is_a?(Array)
+    unless commands.include?(';') || commands.include?('#')
+      commands = "#{commands} $@"
     end
     commands = commands.tr('#', '$')
 
