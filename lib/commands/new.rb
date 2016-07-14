012d2a5df0b40c3a6fafbf36d9fb6594b19194dd
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-07-14 16:32:25 -0300

    Add command new

--- lib/commands/new.rb
+++ lib/commands/new.rb
@@ -20,18 +20,36 @@ class New < Command
   end
 
   def self.run(argv)
-    puts 'NEW'
-    # usage if argv.count < 2
+    check_run(argv)
 
-    # alias_name = argv[0]
-    # commands = argv[1..-1]
-    # result = Mkalias.new_alias(alias_name, commands)
+    alias_name = argv[0]
+    commands = argv[1..-1]
+    result = Mkalias.new_alias(alias_name, commands)
 
-    # if result
-    #   puts " - Created Alias: #{alias_name}"
-    #   check_signal
-    # else
-    #   puts " ERROR: O Alias [#{alias_name}] já existe"
-    # end
+    print_result(result, alias_name)
+  end
+
+  def self.print_result(result, alias_name)
+    if result
+      puts " - Created Alias: #{alias_name}"
+      check_signal
+    else
+      puts " ERROR: O Alias [#{alias_name}] já existe"
+    end
+  end
+
+  def self.check_run(argv)
+    if argv.empty?
+      parent.usage
+      abort
+    end
+  end
+
+  def self.check_signal
+    if Mkalias.signal?
+      `kill -USR1 #{Process.ppid}`
+    else
+      puts " - Run '$ source ~/.bashrc' to use your alias"
+    end
   end
 end
